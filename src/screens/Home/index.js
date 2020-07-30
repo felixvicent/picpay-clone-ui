ceb4ec23fe06f982b2695e498eba36165bc4888e
Félix Vicente <felixvicent1306@gmail.com>
2020-07-30 14:15:58 -0300

    Add tips components

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -4,6 +4,7 @@ import { MaterialCommunityIcons, AntDesign } from '@expo/vector-icons';
 import { Wrapper, Header, BalanceContainer, BalanceTitle, Balance, Container } from './styles'
 import Suggestions from '../../components/Suggestions';
 import Activities from '../../components/Activities';
+import Tips from '../../components/Tips';
 
 export default function Home(){
   return(
@@ -19,6 +20,7 @@ export default function Home(){
         </Header>
         <Suggestions />
         <Activities />
+        <Tips />
       </Container>
     </Wrapper>
   )
